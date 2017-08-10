098058f5eae60cdf89e8d1bb7dc4c4bf6af6d226
Douglas Balkin <dbalkin77@gmail.com>
2017-08-10 12:30:11 -0500

    server js setup

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-var express = require('express');
-var bodyParser = reqiure('body-parser');
-var app = express();
-var path = reqiure('path');
+// Require dependencies so that our app works////////////////////////////////////////////////////
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-app.listen(8000);
+const app = express();
+
+var PORT = process.env.PORT || 8080;
 
 app.get('/', function (req, res){
     res.send('hello world');
+});
+
+// BodyParser so that our server can interpret data sent to it////////////////////////////////////////////////////
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Router -- points to a series of route files////////////////////////////////////////////////////
+// require("./app/routing/apiRoutes")(app);
+// require("./app/routing/htmlRoutes")(app);
+
+// Listener ////////////////////////////////////////////////////
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
