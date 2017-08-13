f96a4ca912ec4ba977858a381290693c0c53de83
Douglas Balkin <dbalkin77@gmail.com>
2017-08-13 10:47:01 -0500

    data added to friend.js file

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,9 +1,21 @@
 
+var tableFriends = [
+{
+  "name":"Chris",
+  "photo":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTQzMTEzOTU5OV5BMl5BanBnXkFtZTcwOTg2ODY3Mg@@._V1_UX214_CR0,0,214,317_AL_.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+    ];
 
-
-
-
-
-
-
-module.exports = friend;
+module.exports = tablefriends;
